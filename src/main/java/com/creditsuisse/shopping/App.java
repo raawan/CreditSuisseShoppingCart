0c00229b748c4bdc330860c908344d7c2c6cc906
raawan <minalfulzele@gmail.com>
2015-04-08 20:32:49 +0100

    initialising the git repository

---
+++ src/main/java/com/creditsuisse/shopping/App.java
@@ -0,0 +1,13 @@
+package com.creditsuisse.shopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
