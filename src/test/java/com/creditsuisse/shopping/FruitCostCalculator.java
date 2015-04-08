eeb01940cfacd5a1c23daf6365470eb68cd0462e
raawan <minalfulzele@gmail.com>
2015-04-09 00:20:33 +0100

    refactored calculateCostForMelons()

--- src/test/java/com/creditsuisse/shopping/FruitCostCalculator.java
+++ src/test/java/com/creditsuisse/shopping/FruitCostCalculator.java
@@ -19,9 +19,6 @@ public class FruitCostCalculator {
 		if(totalMelons%(Melon.TOTAL_MELONS_TO_BUY_TO_GET_A_FREE_MELON+1)!=0){
 			throw new InvalidNumberOfMelonException("There is a free extra melon with every melon. Customer should get one more");
 		}
-		if(totalMelons>=2) {
-			return (totalMelons /2) * Melon.MELON_PRICE;
-		}
-		return 0;
+		return (totalMelons /(Melon.TOTAL_MELONS_TO_BUY_TO_GET_A_FREE_MELON+1)) * Melon.MELON_PRICE;
 	}
 }
