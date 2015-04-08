6d4cbe07ebef9d686a1432fa6699b394edef59e9
raawan <minalfulzele@gmail.com>
2015-04-08 21:38:25 +0100

    test passed; givenBasketWithTwoApplesTwoMelonsAndOneBananaTotalCostReturnedShouldBe140p

--- src/main/java/com/creditsuisse/shopping/ShoppingBasket.java
+++ src/main/java/com/creditsuisse/shopping/ShoppingBasket.java
@@ -5,13 +5,21 @@ public class ShoppingBasket {
 	public int totalCost(String[] itemsInBasket) {
 		
 		int totalCost = 0;
+		int totalMelons = 0;
+		
 		for(String item : itemsInBasket) {
 			if(item.equals("Apple")) {
 				totalCost += 35;
 			}
 			else if (item.equals("Banana")) {
 				totalCost +=20;
+			} else if (item.equals("Melon")) {
+				totalMelons += 1;
 			}
+		}
+		if(totalMelons>=2) {
+			int totalPriceToBeCharged = (totalMelons /2) * 50;
+			totalCost+=totalPriceToBeCharged;
 		}
 		return totalCost;
 	}
