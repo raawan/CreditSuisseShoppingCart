aaf551042460772217bc30869d0cdfb489729e19
raawan <minalfulzele@gmail.com>
2015-04-14 21:48:52 +0100

    added meaningfl names to default

--- src/main/java/com/creditsuisse/shopping/constant/FruitDiscount.java
+++ src/main/java/com/creditsuisse/shopping/constant/FruitDiscount.java
@@ -5,9 +5,10 @@ package com.creditsuisse.shopping.constant;
  */
 public interface FruitDiscount {
 	
+	public static final int NO_DISCOUNT = 0;
 	public static final int TOTAL_LIMES_TO_BUY_TO_GET_A_FREE_LIME = 2;
 	public static final int TOTAL_MELONS_TO_BUY_TO_GET_A_FREE_MELON = 1;
-	public static final int TOTAL_APPLES_TO_BUY_TO_GET_A_FREE_APPLE = 0;
-	public static final int TOTAL_BANANAS_TO_BUY_TO_GET_A_FREE_BANANA = 0;
+	public static final int TOTAL_APPLES_TO_BUY_TO_GET_A_FREE_APPLE = NO_DISCOUNT;
+	public static final int TOTAL_BANANAS_TO_BUY_TO_GET_A_FREE_BANANA = NO_DISCOUNT;
 
 }
